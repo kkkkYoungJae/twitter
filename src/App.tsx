a766ee71272cba2cdf9225249cfd5d0221839cbf
KimYoungJae <dudwodla123@gmail.com>
2023-09-24 19:27:27 +0900

    폴더 구조

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import Router from "components/Router";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>home</h1>} />
-      <Route path="/posts" element={<h1>posts</h1>} />
-      <Route path="/posts:id" element={<h1>posts detail</h1>} />
-      <Route path="/posts/new" element={<h1>posts new</h1>} />
-      <Route path="/posts/edit:id" element={<h1>posts edit</h1>} />
-      <Route path="/profile" element={<h1>profile</h1>} />
-      <Route path="/profile/edit" element={<h1>profile edit</h1>} />
-      <Route path="/notifications" element={<h1>notifications</h1>} />
-      <Route path="/search" element={<h1>search</h1>} />
-
-      <Route path="/users/login" element={<h1>login</h1>} />
-      <Route path="/users/signup" element={<h1>signup</h1>} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  );
+  return <Router />;
 };
 
 export default App;
